3219e1ab0dcd0f46fa6867923df2f81ec86ab2a5
jtG <jude.gyimah87@gmail.com>
2021-02-14 13:54:33 +0100

    cpp node modified

--- src/my_first_node.cpp
+++ src/my_first_node.cpp
@@ -4,9 +4,13 @@ int main(int argc, char **argv)
 {
     ros::init(argc, argv, "my_first_cpp_node");
     ros::NodeHandle nh;
+    ROS_INFO("Node has been started");
 
-    ROS_INFO("Nodehas been started");
-    ros::Duration(1.0).sleep();
+    ros::Rate rate(10);
 
-    ROS_INFO("Exit");
+    while (ros::ok())
+    {
+        ROS_INFO("Hello");
+        rate.sleep();
+    }
 }
